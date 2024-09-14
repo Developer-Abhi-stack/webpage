1d2fe2888e29729fff8fe81e154b0b24d62e7d61
Abhishekkumar2001 <abhikum8131@gmail.com>
2024-09-14 10:42:40 +0530

    bootstrap set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar"
 
 
 const App = () => {
   return (
     <div>
-      <h1>React App</h1>
+    <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className="text-center text-danger">Heading</h1>
+      
+    </div>
+  )
+}
+
+export default Navbar
